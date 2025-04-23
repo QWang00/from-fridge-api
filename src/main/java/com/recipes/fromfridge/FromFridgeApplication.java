69400de372bd613ee593b2399be2278db3f1034d
qiuqiu <qiujuanwang87@gmail.com>
2025-04-23 14:48:23 +0100

    Initial Commit

---
+++ src/main/java/com/recipes/fromfridge/FromFridgeApplication.java
@@ -0,0 +1,13 @@
+package com.recipes.fromfridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FromFridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FromFridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recipes/fromfridge/FromFridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipes.fromfridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FromFridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
