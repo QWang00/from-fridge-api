2fdf78509cc0c24efd470bd56d7ac9c3191a22a3
qiuqiu <qiujuanwang87@gmail.com>
2025-04-24 14:39:26 +0100

    feat: add FridgeIngredient entity with id & ingredient fields

---
+++ src/main/java/com/recipes/fromfridge/model/FridgeIngredient.java
@@ -0,0 +1,21 @@
+package com.recipes.fromfridge.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "fridge_ingredient")
+public class FridgeIngredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "ingredient_id", nullable = false)
+    private Ingredient ingredient;
+}
