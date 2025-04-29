02b4c0c44cea91fa0d1038377091108ded03bc94
qiuqiu <qiujuanwang87@gmail.com>
2025-04-29 10:42:02 +0100

    feat: add findByNameIgnoreCase & getExistingIngredientOrThrow in IngredientService

---
+++ src/main/java/com/recipes/fromfridge/service/IngredientService.java
@@ -0,0 +1,12 @@
+package com.recipes.fromfridge.service;
+
+import com.recipes.fromfridge.model.Ingredient;
+
+import java.util.Optional;
+
+public interface IngredientService {
+
+    Ingredient findByNameIgnoreCase(String name);
+
+    Ingredient getExistingIngredientOrThrow(String name);
+}
