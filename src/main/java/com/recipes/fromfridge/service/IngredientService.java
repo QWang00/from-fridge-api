5f215d6a049638a10821c0f5b08f2335adf46c89
qiuqiu <qiujuanwang87@gmail.com>
2025-04-29 21:14:47 +0100

    refactor: tidy up imports in IngredientService

--- src/main/java/com/recipes/fromfridge/service/IngredientService.java
+++ src/main/java/com/recipes/fromfridge/service/IngredientService.java
@@ -2,8 +2,6 @@ package com.recipes.fromfridge.service;
 
 import com.recipes.fromfridge.model.Ingredient;
 
-import java.util.Optional;
-
 public interface IngredientService {
 
     Ingredient getIngredientByNameIgnoreCase(String name);
