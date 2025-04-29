b1ecae421f129535e5f05823dbb16adc6fc7140b
qiuqiu <qiujuanwang87@gmail.com>
2025-04-29 13:34:29 +0100

    refactor: rename application test name due to the name change of the model FridgeIngredient

--- src/test/java/com/recipes/fromfridge/FromFridgeIngredientApplicationTests.java
+++ src/test/java/com/recipes/fromfridge/FromFridgeIngredientApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FromFridgeApplicationTests {
+class FromFridgeIngredientApplicationTests {
 
 	@Test
 	void contextLoads() {
