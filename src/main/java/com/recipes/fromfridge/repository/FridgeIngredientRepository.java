99866dd141adbc9ff3de4903a37cc72dc51d5a27
qiuqiu <qiujuanwang87@gmail.com>
2025-04-29 21:29:17 +0100

    fix: add repository annotation to FridgeIngredientRepository

--- src/main/java/com/recipes/fromfridge/repository/FridgeIngredientRepository.java
+++ src/main/java/com/recipes/fromfridge/repository/FridgeIngredientRepository.java
@@ -2,8 +2,11 @@ package com.recipes.fromfridge.repository;
 
 import com.recipes.fromfridge.model.FridgeIngredient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface FridgeIngredientRepository extends JpaRepository<FridgeIngredient, Integer> {
 
     // Check if an ingredient is already in fridge
