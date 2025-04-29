619c0d6ac000023b0f751f036dc486772d9fc8f7
qiuqiu <qiujuanwang87@gmail.com>
2025-04-29 15:17:05 +0100

    feat: implement removeIngredientFromFridge in FridgeIngredientController layer

--- src/main/java/com/recipes/fromfridge/controller/FridgeIngredientController.java
+++ src/main/java/com/recipes/fromfridge/controller/FridgeIngredientController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -23,6 +24,12 @@ public class FridgeIngredientController {
     @GetMapping("/ingredients")
     public ResponseEntity<List<FridgeIngredient>> getAllFridgeIngredients() {
         return new ResponseEntity<>(fridgeIngredientService.getAllFridgeIngredients(), HttpStatus.OK);
+    }
+
+    @GetMapping("/ingredient/{id}")
+    public ResponseEntity<String> removeIngredientFromFridge (@PathVariable("id") Integer id){
+        fridgeIngredientService.removeIngredientFromFridge(id);
+        return new ResponseEntity<>("Ingredient removed from fridge.", HttpStatus.OK);
     }
 
 }
