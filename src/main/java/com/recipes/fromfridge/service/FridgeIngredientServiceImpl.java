34b5009116bab6c5e878a1ac6873f225e54f5080
qiuqiu <qiujuanwang87@gmail.com>
2025-04-29 13:36:44 +0100

    feat: implement addIngredientToFridge in FridgeIngredientImpl

---
+++ src/main/java/com/recipes/fromfridge/service/FridgeIngredientServiceImpl.java
@@ -0,0 +1,32 @@
+package com.recipes.fromfridge.service;
+
+import com.recipes.fromfridge.exception.DuplicateItemException;
+import com.recipes.fromfridge.model.FridgeIngredient;
+import com.recipes.fromfridge.model.Ingredient;
+import com.recipes.fromfridge.repository.FridgeIngredientRepository;
+
+public class FridgeIngredientServiceImpl implements FridgeIngredientService{
+
+    private final FridgeIngredientRepository fridgeIngredientRepository;
+    private final IngredientServiceImpl ingredientService;
+
+    public FridgeIngredientServiceImpl(FridgeIngredientRepository fridgeIngredientRepository, IngredientServiceImpl ingredientService) {
+        this.fridgeIngredientRepository = fridgeIngredientRepository;
+        this.ingredientService = ingredientService;
+    }
+
+    @Override
+    public void addIngredientToFridge(String ingredientName) {
+        Ingredient ingredient = ingredientService.getIngredientByNameIgnoreCase(ingredientName);
+        Integer ingredientId = ingredient.getId();
+        boolean ingredientInFridge = fridgeIngredientRepository.existsByIngredientId(ingredientId);
+        if(ingredientInFridge){
+            throw new DuplicateItemException(
+                    String.format("Ingredient [%s] already exists in the fridge.", ingredientName)
+            );
+        }
+        FridgeIngredient fridgeIngredient = new FridgeIngredient();
+        fridgeIngredient.setIngredient(ingredient);
+        fridgeIngredientRepository.save(fridgeIngredient);
+    }
+}
