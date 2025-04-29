cceccf0a83fc8bf8208e6c5c8e880de7b32a0716
qiuqiu <qiujuanwang87@gmail.com>
2025-04-29 13:42:13 +0100

    feat: add getAll, remove, clear methods in FridgeIngredientService

--- src/main/java/com/recipes/fromfridge/service/FridgeIngredientService.java
+++ src/main/java/com/recipes/fromfridge/service/FridgeIngredientService.java
@@ -1,7 +1,17 @@
 package com.recipes.fromfridge.service;
 
+import com.recipes.fromfridge.model.FridgeIngredient;
+
+import java.util.List;
+
 public interface FridgeIngredientService {
 
     void addIngredientToFridge(String ingredientName);
+
+    List<FridgeIngredient> getAllFridgeIngredients();
+
+    void removeIngredientFromFridge(Long fridgeIngredientId);
+
+    void clearFridge();
 
 }
