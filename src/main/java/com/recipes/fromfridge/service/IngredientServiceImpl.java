f0537f86f8f750e39481507033adb585ee87ee88
qiuqiu <qiujuanwang87@gmail.com>
2025-04-29 10:43:14 +0100

    feat: implement findByNameIgnoreCase in IngredientServiceImpl

---
+++ src/main/java/com/recipes/fromfridge/service/IngredientServiceImpl.java
@@ -0,0 +1,29 @@
+package com.recipes.fromfridge.service;
+
+import com.recipes.fromfridge.exception.ItemNotFoundException;
+import com.recipes.fromfridge.model.Ingredient;
+import com.recipes.fromfridge.repository.IngredientRepository;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+public class IngredientServiceImpl implements IngredientService {
+    private final IngredientRepository ingredientRepository;
+
+    public IngredientServiceImpl(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
+
+    @Override
+    public Ingredient findByNameIgnoreCase(String name) {
+         return ingredientRepository.findByNameIgnoreCase(name.trim())
+                 .orElseThrow(()-> new ItemNotFoundException(
+                         String.format("Ingredient [%s] not recognized, please try again")));
+    }
+
+    @Override
+    public Ingredient getExistingIngredientOrThrow(String name) {
+        return null;
+    }
+}
