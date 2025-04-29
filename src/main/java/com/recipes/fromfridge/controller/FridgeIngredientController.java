14d07c6bd05f1f8ea676c453b105f80542fe3b4b
qiuqiu <qiujuanwang87@gmail.com>
2025-04-29 20:00:12 +0100

    fix: correct mapping annotation for removeIngredientFromFridge in FridgeIngredientController

--- src/main/java/com/recipes/fromfridge/controller/FridgeIngredientController.java
+++ src/main/java/com/recipes/fromfridge/controller/FridgeIngredientController.java
@@ -5,10 +5,7 @@ import com.recipes.fromfridge.service.FridgeIngredientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
@@ -26,10 +23,11 @@ public class FridgeIngredientController {
         return new ResponseEntity<>(fridgeIngredientService.getAllFridgeIngredients(), HttpStatus.OK);
     }
 
-    @GetMapping("/ingredient/{id}")
+    @DeleteMapping("/ingredient/{id}")
     public ResponseEntity<String> removeIngredientFromFridge (@PathVariable("id") Integer id){
         fridgeIngredientService.removeIngredientFromFridge(id);
         return new ResponseEntity<>("Ingredient removed from fridge.", HttpStatus.OK);
     }
+
 
 }
