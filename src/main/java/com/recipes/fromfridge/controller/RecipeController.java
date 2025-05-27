70334d7c30361632128d7fd7ea65519848d08505
qiuqiu <qiujuanwang87@gmail.com>
2025-05-27 13:10:34 +0100

    feat: implement searchRecipesByIngredientNames in RecipeController

---
+++ src/main/java/com/recipes/fromfridge/controller/RecipeController.java
@@ -0,0 +1,31 @@
+package com.recipes.fromfridge.controller;
+
+import com.recipes.fromfridge.model.Recipe;
+import com.recipes.fromfridge.service.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/from-fridge/recipes")
+public class RecipeController {
+    private final RecipeService recipeService;
+
+    @Autowired
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Recipe>> searchRecipesByIngredientNames(@RequestParam List<String> ingredientNames){
+        if (ingredientNames == null || ingredientNames.isEmpty()) {
+            throw new IllegalArgumentException("Please provide at least one ingredient.");
+        }
+        return new ResponseEntity<>(recipeService.searchRecipesByIngredientNames(ingredientNames), HttpStatus.OK);
+    }
+}
