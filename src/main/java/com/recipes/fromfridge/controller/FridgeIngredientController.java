3babd511decf4ca72634404ce7da4e233a0fcb5a
qiuqiu <qiujuanwang87@gmail.com>
2025-05-27 13:00:48 +0100

    refactor: add app name to endpoint address in FridgeIngredientController

--- src/main/java/com/recipes/fromfridge/controller/FridgeIngredientController.java
+++ src/main/java/com/recipes/fromfridge/controller/FridgeIngredientController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/fridge")
+@RequestMapping("/api/v1/from-fridge/fridge")
 public class FridgeIngredientController {
     private final FridgeIngredientService fridgeIngredientService;
 
