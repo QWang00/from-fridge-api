0d2e37ef6e6fc798e98b86f942347db387ca0450
qiuqiu <qiujuanwang87@gmail.com>
2025-06-03 10:13:14 +0100

    test: add tests for when more than 5 ingredients provided for searchRecipesByIngredientNames in RecipeServiceImplTest

---
+++ src/test/java/com/recipes/fromfridge/service/RecipeServiceImplTest.java
@@ -0,0 +1,52 @@
+package com.recipes.fromfridge.service;
+
+import com.recipes.fromfridge.exception.DuplicateItemException;
+import com.recipes.fromfridge.model.Ingredient;
+import com.recipes.fromfridge.repository.RecipeRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Nested;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class RecipeServiceImplTest {
+
+    @Mock
+    private RecipeRepository recipeRepository;
+
+    @Mock
+    private IngredientServiceImpl ingredientService;
+
+    @InjectMocks
+    private RecipeServiceImpl recipeService;
+
+    @Nested
+    class SearchRecipesByIngredientNames  {
+
+        @Test
+        @DisplayName("Should throw IllegalArgumentException when more than 5 ingredients are entered")
+        void moreThanFiveIngredientsProvided() {
+            List<String> ingredients = List.of("egg", "milk", "flour", "sugar", "butter", "salt");
+
+            IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+                recipeService.searchRecipesByIngredientNames(ingredients);
+            });
+
+            assertEquals("You can enter up to 5 ingredients.", exception.getMessage());
+        }
+
+
+
+
+
+
+
+    }
+
+}
