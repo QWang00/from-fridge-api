b1fc521c205517deb236bb6b336116ff235060f1
qiuqiu <qiujuanwang87@gmail.com>
2025-06-03 15:03:56 +0100

    feat: add getRecipeDetailById in RecipeService

--- src/main/java/com/recipes/fromfridge/service/RecipeService.java
+++ src/main/java/com/recipes/fromfridge/service/RecipeService.java
@@ -1,10 +1,13 @@
 package com.recipes.fromfridge.service;
 
+import com.recipes.fromfridge.dto.RecipeDetailDto;
 import com.recipes.fromfridge.dto.RecipePreviewResponse;
 import java.util.List;
 
 public interface RecipeService {
 
     List<RecipePreviewResponse> searchRecipesByIngredientNames(List<String> ingredientNames);
+
+    RecipeDetailDto getRecipeDetailById(Integer recipeId, List<String> ownedIngredients);
 
 }
