2e303f3cf27e9ac3f5357b99a3ad7bc2d5d0ce51
qiuqiu <qiujuanwang87@gmail.com>
2025-06-12 20:06:25 +0100

    feat: add findByNameContainingIgnoreCase in IngredientService

--- src/main/java/com/recipes/fromfridge/service/IngredientService.java
+++ src/main/java/com/recipes/fromfridge/service/IngredientService.java
@@ -2,8 +2,13 @@ package com.recipes.fromfridge.service;
 
 import com.recipes.fromfridge.model.Ingredient;
 
+import java.util.List;
+
 public interface IngredientService {
 
     Ingredient getIngredientByNameIgnoreCase(String name);
+
+    List<Ingredient> searchIngredients(String query);
+
 
 }
