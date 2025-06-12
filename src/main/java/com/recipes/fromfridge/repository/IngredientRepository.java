fd45ba64db0092b291f20d01f8dccc04fdbfb7b8
qiuqiu <qiujuanwang87@gmail.com>
2025-06-12 20:06:10 +0100

    feat: add findByNameContainingIgnoreCase in IngredientRepository

--- src/main/java/com/recipes/fromfridge/repository/IngredientRepository.java
+++ src/main/java/com/recipes/fromfridge/repository/IngredientRepository.java
@@ -3,10 +3,15 @@ package com.recipes.fromfridge.repository;
 import com.recipes.fromfridge.model.Ingredient;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface IngredientRepository extends CrudRepository<Ingredient, Integer> {
     Optional<Ingredient> findByNameIgnoreCase(String name);
+
+    List<Ingredient> findByNameContainingIgnoreCase(String query);
+
 
 }
